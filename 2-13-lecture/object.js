a4c2e3b94b3101006e1c8d05161c50e2eeaf15b3
Nathan Hishon <nathan.hishon@gmail.com>
2025-02-10 15:48:10 -0500

    2 10 lecture

---
+++ 2-13-lecture/object.js
@@ -0,0 +1,20 @@
+// we can create an object with the template literal syntax
+const cat = {
+    name: 'Pinecone',
+    color: 'grey',
+    type: 'long hair',
+    meow: function () {
+        console.log(this.name)
+    },
+    meow2: () => {
+        console.log(this.name)
+    }
+}
+
+// console.log(cat.name)
+cat.meow()
+cat['meow']()
+// cat.meow2()
+// console.log(this)
+console.log(window)
+console.log(document)
